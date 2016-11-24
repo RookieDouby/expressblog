f695e1f201662901525d757a9fa8403c37bb6754
RookieDouby <zhd1109@qq.com>
2016-11-25 01:19:20 +0800

    add login logout function

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,20 @@ var User = require('../models/user.js');
 
 module.exports = function(app) {
   app.get('/', function (req, res) {
-    res.render('index', { title: '主页' });
+    res.render('index', {
+        title: 'Homepage' ,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+    });
   });
   app.get('/reg', function (req, res) {
-    res.render('reg', { title: '注册' });
+    res.render('reg', {
+        title: 'Register',
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+    });
   });
   app.post('/reg', function (req, res) {
 		var name = req.body.name,
@@ -14,7 +24,7 @@ module.exports = function(app) {
 			password_re = req.body['password-repeat'];
 		//检验用户两次输入的密码是否一致
 		if (password_re != password) {
-			req.flash('error', '两次输入的密码不一致!'); 
+			req.flash('error', '两次输入的密码不一致!');
 			return res.redirect('/reg');//返回注册页
 		}
 		//生成密码的 md5 值
@@ -25,7 +35,7 @@ module.exports = function(app) {
 			password: password,
 			email: req.body.email
 		});
-		//检查用户名是否已经存在 
+		//检查用户名是否已经存在
 		User.get(newUser.name, function (err, user) {
 			if (err) {
 			req.flash('error', err);
@@ -47,13 +57,39 @@ module.exports = function(app) {
 			});
 		});
 	});
+
   app.get('/login', function (req, res) {
-    res.render('login', { title: '登录' });
+      res.render('login', {
+          title: '登录',
+          user: req.session.user,
+          success: req.flash('success').toString(),
+          error: req.flash('error').toString()});
   });
   app.post('/login', function (req, res) {
+       //生成密码的 md5 值
+       var md5 = crypto.createHash('md5'),
+           password = md5.update(req.body.password).digest('hex');
+       //检查用户是否存在
+       User.get(req.body.name, function (err, user) {
+         if (!user) {
+           req.flash('error', 'User is not existed!');
+           return res.redirect('/login');//用户不存在则跳转到登录页
+         }
+         //检查密码是否一致
+         if (user.password != password) {
+           req.flash('error', 'Password not passed!');
+           return res.redirect('/login');//密码错误则跳转到登录页
+         }
+         //用户名密码都匹配后，将用户信息存入 session
+         req.session.user = user;
+         req.flash('success', 'Login successfully!');
+         res.redirect('/');//登陆成功后跳转到主页
+       });
   });
   app.get('/post', function (req, res) {
-    res.render('post', { title: '发表' });
+      req.session.user = null;
+      req.flash('success','Logout successfully');
+      res.redirect('/');
   });
   app.post('/post', function (req, res) {
   });

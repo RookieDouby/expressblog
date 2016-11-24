7791e3f992c46e88ff277c774d10ddb61a72db70
sam zhang <szhang@movoto.com>
2016-11-24 15:02:02 +0800

    add login function

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,20 @@ var User = require('../models/user.js');
 
 module.exports = function(app) {
   app.get('/', function (req, res) {
-    res.render('index', { title: '主页' });
+    res.render('index', { 
+		title: '主页',
+		user: req.session.user,
+		success: req.flash('success').toString(),
+		error: req.flash('error').toString()
+	 });
   });
   app.get('/reg', function (req, res) {
-    res.render('reg', { title: '注册' });
+    res.render('reg', { 
+		title: '注册',
+		user: req.session.user,
+		success: req.flash('success').toString(),
+		error: req.flash('error').toString()
+	 });
   });
   app.post('/reg', function (req, res) {
 		var name = req.body.name,
@@ -42,15 +52,39 @@ module.exports = function(app) {
 				return res.redirect('/reg');//注册失败返回主册页
 			}
 			req.session.user = user;//用户信息存入 session
-			req.flash('success', '注册成功!');
+			req.flash('success', 'Registered successfully!');
 			res.redirect('/');//注册成功后返回主页
 			});
 		});
 	});
   app.get('/login', function (req, res) {
-    res.render('login', { title: '登录' });
+    res.render('login', { 
+		title: '登录',
+		user: req.session.user,
+		success: req.flash('success').toString(),
+		error: req.flash('error').toString()	
+ 	});
   });
   app.post('/login', function (req, res) {
+	  //create a md5 password
+	  var md5 = crypto.createHash('md5');
+	  var password = md5.update(req.body.password).digest('hex');
+	  //check if user has been created
+	  User.get(req.body.name, function(err, user) {
+		  if (!user) {
+			  req.flash('error','user don\'t exist');
+			  return res.redirect('/login');
+		  }
+		  //check password
+		  if (user.password != password) {
+			  req.flash('error','password not correct');
+			  return res.redirect('/login');
+		  }
+		  //password access save user into session
+		  req.session.user = user;
+		  req.flash('success','Login successfully');
+		  res.redirect('/');
+	  });
   });
   app.get('/post', function (req, res) {
     res.render('post', { title: '发表' });
